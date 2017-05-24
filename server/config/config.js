96011012ab4c0ef945aedc7192231128afeea749
Btanwani77 <Btanwani77@gmail.com>
2017-05-24 12:43:01 +0530

    database connection url tweak

--- server/config/config.js
+++ server/config/config.js
@@ -3,7 +3,8 @@ var env = process.env.NODE_ENV || 'development';
 
 if(env === 'development') {
   process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+  // process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+  process.env.MONGODB_URI = 'mongodb://Jayant:Jayant@ds149501.mlab.com:49501/todoapp';
 } else if(env === 'test') {
   process.env.PORT = 4000;
   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
