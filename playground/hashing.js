d37b800ab49e23588ae86f1565e81c4f5c9f7d77
Btanwani77 <Btanwani77@gmail.com>
2017-05-28 12:48:47 +0530

    Add POST /users/login route

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,17 +1,31 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+var password = '123abc!';
 
-var data = {
-  id:10
-};
+bcrypt.genSalt(10,(err,salt) => {
+  bcrypt.hash(password,salt,(err,hash) => {
+    console.log(hash);
+  });
+});
+
+var hashedPassword = '$2a$10$Hnc1zPbCmxmw7EzC4b1Gj.rPOzR6A8YdIH/fhwxMEEqZy3negQfvy'
+
+bcrypt.compare('123abc',hashedPassword,(err,res) => {
+  console.log(res);
+});
+
+// var data = {
+//   id:10
+// };
 
 
-var token = jwt.sign(data,'123abc');
-// jwt.verify();
-console.log(token);
-
-var decoded = jwt.verify(token,'123abc')
-console.log(decoded);
+// var token = jwt.sign(data,'123abc');
+// // jwt.verify();
+// console.log(token);
+//
+// var decoded = jwt.verify(token,'123abc')
+// console.log(decoded);
 // var message = 'I am user number 3';
 // var hash = SHA256(message).toString();
 // console.log(message);
